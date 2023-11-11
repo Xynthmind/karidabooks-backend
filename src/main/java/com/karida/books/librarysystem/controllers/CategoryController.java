2c6693c9b8d60d278a5d27efe5e4509d4cf5cda5
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-11 12:36:54 -0600

    CategoryController file created with all imports and request link for the postman

--- src/main/java/com/karida/books/librarysystem/controllers/CategoryController.java
+++ src/main/java/com/karida/books/librarysystem/controllers/CategoryController.java
@@ -1,4 +1,14 @@
 package com.karida.books.librarysystem.controllers;
+import com.karida.books.librarysystem.models.Category;
+import com.karida.books.librarysystem.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/library/categories")
 public class CategoryController {
 }
