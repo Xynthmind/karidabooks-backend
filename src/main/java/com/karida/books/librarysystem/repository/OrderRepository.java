ebe61d7e84ee498eead5f0a62baf41c28b351924
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-12 01:53:28 -0600

    REST finished for Orders

---
+++ src/main/java/com/karida/books/librarysystem/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.karida.books.librarysystem.repository;
+
+import org.springframework.stereotype.Repository;
+import com.karida.books.librarysystem.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    @Query(value = "SELECT * FROM orders WHERE id_user = ?1", nativeQuery = true)
+    List<Order> findById_User(Long id);
+}
