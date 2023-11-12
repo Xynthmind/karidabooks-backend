116a0e063e4568dca8539b8af77e072579d0bb44
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-12 17:55:14 -0600

    REST operations added

---
+++ src/main/java/com/karida/books/librarysystem/repository/OrderDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.karida.books.librarysystem.repository;
+import com.karida.books.librarysystem.models.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{
+    @Query(value = "SELECT * FROM ordersdetails WHERE id_order = ?1", nativeQuery = true)
+    List<OrderDetails> finByOrder(Long id);
+}
