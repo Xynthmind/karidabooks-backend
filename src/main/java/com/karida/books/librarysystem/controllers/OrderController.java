2c3c19c974f10e7466ea1253211fceb651b8437a
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-16 23:24:04 -0600

    CORDS support added

--- src/main/java/com/karida/books/librarysystem/controllers/OrderController.java
+++ src/main/java/com/karida/books/librarysystem/controllers/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
+@CrossOrigin
 @RequestMapping("/library/orders")
 public class OrderController {
     @Autowired
